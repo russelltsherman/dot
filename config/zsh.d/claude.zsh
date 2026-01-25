c8aae8f808c21f4b01851dbe29e67365a701161e
russell sherman <russell@archiveresale.co>
2026-01-25 11:23:11 -0600

    add PAI

--- config/zsh.d/claude.zsh
+++ config/zsh.d/claude.zsh
@@ -1,2 +1,6 @@
 
-export CLAUDE_CONFIG_DIR="$XDG_CONFIG_HOME/claude"
+# export CLAUDE_CONFIG_DIR="$XDG_CONFIG_HOME/.claude"
+
+export CLAUDE_CONFIG_DIR="$HOME/.claude"
+export PAI_DIR="$CLAUDE_CONFIG_DIR"
+alias pai='bun ~/.claude/skills/CORE/Tools/pai.ts'
