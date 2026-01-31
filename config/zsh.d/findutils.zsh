d3b8fb6326dafa430db36ad403371dc1a75df749
russell sherman <russell@archiveresale.co>
2026-01-31 17:30:22 -0600

    document gnu replacements

--- config/zsh.d/findutils.zsh
+++ config/zsh.d/findutils.zsh
@@ -1,5 +1,12 @@
-
+# if findutils is installed place it at the front of the path so 
+# its versions will be found before bsd based versions
 if [ -d "/opt/homebrew/opt/findutils/libexec/gnubin" ]
 then
   PATH="/opt/homebrew/opt/findutils/libexec/gnubin:$PATH"
 fi
+
+#  find -> ../../bin/gfind
+#  locate -> ../../bin/glocate
+#  man -> ../gnuman
+#  updatedb -> ../../bin/gupdatedb
+#  xargs -> ../../bin/gxargs
--- config/zsh.d/ggrep.zsh
+++ config/zsh.d/ggrep.zsh
@@ -4,3 +4,8 @@ if chk::command 'ggrep'
 then
   export PATH="/opt/homebrew/opt/grep/libexec/gnubin:$PATH"
 fi
+
+# egrep -> ../../bin/gegrep
+# fgrep -> ../../bin/gfgrep
+# grep -> ../../bin/ggrep
+# man -> ../gnuman
