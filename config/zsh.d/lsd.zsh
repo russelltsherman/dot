3a70cbce9bb04587aca048395d0b48d3c3a6323d
russell sherman <russell@archiveresale.co>
2026-01-31 17:31:27 -0600

    if lsd is not installed still create ls aliases

--- config/zsh.d/lsd.zsh
+++ config/zsh.d/lsd.zsh
@@ -3,8 +3,14 @@
 
 if chk::command "lsd"
 then
+  echo "lsd is installed"
   alias ls='lsd'
   alias ll='lsd -l'
   alias lla='lsd -a'
   alias llal='lsd -al'
+else
+  echo "lsd is not installed"
+  alias ll='ls -l'
+  alias lla='ls -a'
+  alias llal='ls -al'
 fi
