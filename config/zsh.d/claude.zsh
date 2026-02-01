1321304b508a7478c923fdfcf34df997f72259c9
russell sherman <russell@archiveresale.co>
2026-02-01 08:34:24 -0600

    decouple claude and pai from dot

--- config/zsh.d/claude.zsh
+++
@@ -1,7 +0,0 @@
-
-# set default claude directory
-export CLAUDE_CONFIG_DIR="$XDG_CONFIG_HOME/claude"
-
-# set default pai directory
-export PAI_CONFIG_DIR="$XDG_CONFIG_HOME/assistant"
-alias pai="CLAUDE_CONFIG_DIR=$PAI_CONFIG_DIR bun ${PAI_CONFIG_DIR}/skills/CORE/Tools/pai.ts"
