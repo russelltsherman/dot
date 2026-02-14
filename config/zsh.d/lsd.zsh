c180f0891d6e4dd1f7068cbefec2b2646f4ff2ea
russell sherman <russell@archiveresale.co>
2026-02-14 13:51:38 -0600

    remove debug echos

--- config/zsh.d/lsd.zsh
+++ config/zsh.d/lsd.zsh
@@ -3,13 +3,11 @@
 
 if chk::command "lsd"
 then
-  echo "lsd is installed"
   alias ls='lsd'
   alias ll='lsd -l'
   alias lla='lsd -a'
   alias llal='lsd -al'
 else
-  echo "lsd is not installed"
   alias ll='ls -l'
   alias lla='ls -a'
   alias llal='ls -al'
