8381b798241d5f0c968decb390b595f2c8456770
Russell Sherman <russell.t.sherman@gmail.com>
2026-05-27 09:11:10 -0500

    github favor using gh oauth

--- config/zsh.d/github.zsh
+++ config/zsh.d/github.zsh
@@ -2,14 +2,17 @@
 export GITHUB_PROFILE="$(active_profile)"
 export GITHUB_TOKEN_FILE="$XDG_CONFIG_HOME/github/$GITHUB_PROFILE"
 
-if [[ -f "$GITHUB_TOKEN_FILE" ]]
-then
-  GITHUB_TOKEN="$(head -n 1 $GITHUB_TOKEN_FILE)"
-  export GITHUB_TOKEN
-  export GITHUB_API_TOKEN=$GITHUB_TOKEN
-  export GITHUB_PERSONAL_ACCESS_TOKEN=$GITHUB_API_TOKEN
-else
-  unset GITHUB_TOKEN
-  unset GITHUB_API_TOKEN
-  unset GITHUB_PERSONAL_ACCESS_TOKEN
-fi
+export GH_TOKEN=$(gh auth token)
+
+# if [[ -f "$GITHUB_TOKEN_FILE" ]]
+# then
+#   GH_TOKEN="$(head -n 1 $GITHUB_TOKEN_FILE)"
+#   export GH_TOKEN
+#   export GITHUB_TOKEN=$GH_TOKEN
+#   export GITHUB_API_TOKEN=$GITHUB_TOKEN
+#   export GITHUB_PERSONAL_ACCESS_TOKEN=$GITHUB_API_TOKEN
+# else
+#   unset GITHUB_TOKEN
+#   unset GITHUB_API_TOKEN
+#   unset GITHUB_PERSONAL_ACCESS_TOKEN
+# fi
